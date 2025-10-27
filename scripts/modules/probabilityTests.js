94eb38461fff75ed0f152383826d233fb0526951
artist1970 <ArtistJenPearl@gmail.com>
2025-10-27 11:35:14 -0400

    Create probabilityTests.js

---
+++ scripts/modules/probabilityTests.js
@@ -0,0 +1,7 @@
+export const probabilityTests = [
+  { id: 1, question: "A die is rolled. Probability of getting an even number?", options:{A:"1/6", B:"1/2", C:"1/3", D:"2/3"}, answer:"B" },
+  { id: 2, question: "A card is drawn. Probability of drawing a king or queen?", options:{A:"1/13", B:"1/26", C:"2/13", D:"1/6"}, answer:"C" },
+  { id: 3, question: "A coin is flipped twice. Probability of getting two heads?", options:{A:"1/2", B:"1/4", C:"1/3", D:"1/8"}, answer:"B" },
+  { id: 4, question: "Two dice rolled. Probability sum is 7?", options:{A:"1/6", B:"1/8", C:"1/12", D:"1/9"}, answer:"A" }
+  // ...continue until 100 probability questions...
+];
