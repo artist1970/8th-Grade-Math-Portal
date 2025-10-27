7f2371722b90aa6fdf2704ab85f54e7d9402a495
artist1970 <ArtistJenPearl@gmail.com>
2025-10-27 11:35:45 -0400

    Create problemSolvingTests.js

---
+++ scripts/modules/problemSolvingTests.js
@@ -0,0 +1,6 @@
+export const problemSolvingTests = [
+  { id: 1, question: "Twice a number is 14. Find the number.", options:{A:"6", B:"7", C:"8", D:"9"}, answer:"B" },
+  { id: 2, question: "A number plus 5 equals 12. Find the number.", options:{A:"6", B:"7", C:"8", D:"9"}, answer:"B" },
+  { id: 3, question: "Three less than a number is 10. Find the number.", options:{A:"12", B:"13", C:"14", D:"15"}, answer:"B" }
+  // ...continue until 100 problem solving questions...
+];
